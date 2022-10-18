fa6bd017c562ee084b03044939195c2c5a47f392
Ceribe <ceribekagami@gmail.com>
2022-10-18 23:42:03 +0200

    Add region selection to stats

--- app/src/main/java/com/kagamiapps/tofassistant/ui/jo_drop_stats/JODropStatsViewModel.kt
+++ app/src/main/java/com/kagamiapps/tofassistant/ui/jo_drop_stats/JODropStatsViewModel.kt
@@ -3,10 +3,12 @@ package com.kagamiapps.tofassistant.ui.jo_drop_stats
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kagamiapps.tofassistant.data.JOLootRepository
+import com.kagamiapps.tofassistant.data.consts.Region
+import com.kagamiapps.tofassistant.ui.jo_loot_list.JOLootListEvent
+import com.kagamiapps.tofassistant.util.Routes
+import com.kagamiapps.tofassistant.util.UiEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,19 +18,34 @@ class JODropStatsViewModel @Inject constructor(
 
     private val loots = repository.getLoots().map { it.asReversed() }
 
-    val numberOfChestsSinceLastDropOfType = loots
+    val region = MutableStateFlow(Region.Aesperia)
+
+    private val filteredLoots = combine(loots, region) { loots, region ->
+        loots.filter { it.jo.region == region }
+    }
+
+    val numberOfChestsSinceLastDropOfType = filteredLoots
         .map(LootAnalyzer::calculateNumberOfChestsSinceLastDropOfType)
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyMap())
 
-    val totalDropsOfType = loots
+    val totalDropsOfType = filteredLoots
         .map(LootAnalyzer::calculateTotalDropsOfType)
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyMap())
 
-    val averageNumberOfDropsOfType = loots
+    val averageNumberOfDropsOfType = filteredLoots
         .map(LootAnalyzer::calculateAverageNumberOfDropsOfType)
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyMap())
 
-    val longestStreakWithoutDropOfType = loots
+    val longestStreakWithoutDropOfType = filteredLoots
         .map(LootAnalyzer::calculateLongestStreakWithoutDropOfType)
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyMap())
+
+
+    fun onEvent(event: JODropStatsEvent) {
+        when(event) {
+            is JODropStatsEvent.OnRegionChange -> {
+                region.value = event.region
+            }
+        }
+    }
 }
